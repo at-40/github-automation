cfe1542ee4c09f205f25d0a4eacb2adf43fb696f
at-40 <atul.tiwari.9955@gmail.com>
2025-07-04 22:48:08 +0530

    Added tf code for cs50-python repository

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ resource "github_repository" "ops-core" {
  visibility  = "public"
  auto_init   = true
 }
+
+
+resource "github_repository" "cs50-python" {
+ name        = "cs50-python"
+ description = "Repository for my notes and solutions of CS50 Python"
+ visibility  = "public"
+ auto_init   = true
+}
